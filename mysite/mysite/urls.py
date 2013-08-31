968314f6541db38e02039ab0b065de33b2852c2f
Saravanan Sellathurai <saravanan@saravanan.local>
2013-08-31 03:20:44 +0200

    changes to urlpatterns

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,5 @@ urlpatterns = patterns('',
     url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     #url(r'^newtime/$', new_datetime),
     url(r'^polls/', include('polls.urls')),
-    url(r'^hook', include('github_hook.urls')),
+    url(r'^hook/', include('github_hook.urls')),
 )
